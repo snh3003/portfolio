2c0bfd3299e98f1b376eb91f137bc79cb8e60c6c
snh3003 <shahsama542@gmail.com>
2020-05-31 21:14:05 +0530

    Updates

---
+++ precache-manifest.e9bf3d0414e32115871755ba4ccda6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cec2b0e32ae798c5c6c3971301b2728",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c3bd10efc0a4bd0403ac",
+    "url": "/portfolio/static/css/main.8bd3102d.chunk.css"
+  },
+  {
+    "revision": "bf49fce1133a8d9f838e",
+    "url": "/portfolio/static/js/2.d108c997.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.d108c997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3bd10efc0a4bd0403ac",
+    "url": "/portfolio/static/js/main.a763658d.chunk.js"
+  },
+  {
+    "revision": "a82b76e3079a616b48b5",
+    "url": "/portfolio/static/js/runtime-main.ab3d5e0f.js"
+  }
+]);
